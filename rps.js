91e154adae47334116bb7516dad9de605ed3e8a9
cbrcrash <6812811+cbrcrash@users.noreply.github.com>
2023-04-01 20:52:24 -0700

    Completed Rock Paper Sissors game with 5 rounds

--- rps.js
+++ rps.js
@@ -1,39 +1,50 @@
-const roundOutcome = [
-    [["Tie", 0],[ "paper beats rock", 2], ["rock beats sissors", 1]],
-    [["paper beats rock", 1], ["Tie", 0], ["sissors beats paper", 2]],
-    [["rock beats sissors", 2], ["sissors beats paper", 1], ["Tie", 0]]
-];
+// Define the choices and scores
+const choices = ["rock", "paper", "scissors"];
+let wins = 0;
+let losses = 0;
 
-const choice = ["rock", "paper", "sissors"];
-
-var n2c = {
-    0: "rock",
-    1: "paper",
-    2: "sissors"
-};
-
-var c2n = {
-    "rock": 0,
-    "paper": 1,
-    "sissors": 2
-};
-
-function getComputerChoice() {
-    let cchoice = Math.floor(Math.random() * 3);
-    return cchoice
+// Update the score display
+function updateScore() {
+  document.getElementById("wins").textContent = wins;
+  document.getElementById("losses").textContent = losses;
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    let pchoice = c2n[playerSelection]
-    console.log(`Player chose ${n2c[pchoice]}`);
-    let cchoice = getComputerChoice();
-    console.log(`Computer chose ${n2c[cchoice]}`);
-    outcome = roundOutcome[pchoice][cchoice]
-    let result = `${outcome[0]} ${outcome[1] > 0 ? outcome[1] + " won round!":outcome[1] + " round!"}`
-    return result
+function resetScore() {
+  wins = 0;
+  losses = 0;
 }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+
+// Play the game
+function playGame(playerChoice) {
+  // Get the computer's choice
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  let choice = `You chose ${playerChoice}, Computer chose ${computerChoice}, `;
+
+  // Determine the winner
+  if (playerChoice === computerChoice) {
+      document.getElementById("messagebox").textContent = `You both chose ${computerChoice}, Tie round!`;
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    wins++;
+    if (wins < 5) {
+      document.getElementById("messagebox").textContent = `${choice} You won this round!`;
+    } else {
+      document.getElementById("messagebox").textContent = `${choice} You won this game!`;
+      resetScore()
+    }
+  } else {
+    losses++;
+    if (losses < 5) {
+      document.getElementById("messagebox").textContent = `${choice} Computer won this round!`;
+    } else {
+      document.getElementById("messagebox").textContent = `${choice} Computer won this game!`;
+      resetScore()
+    }
+  }
+
+  // Update the score display
+  updateScore();
+}
